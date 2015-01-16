f88227ce2771b3b8cb0ae3ae55d0e4e5f3b1ecbb
Alan Meadows <alan.meadows@gmail.com>
2015-01-15 16:44:42 -0800

    bump metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       'openstack-chef'
 maintainer_email 'opscode-chef-openstack@googlegroups.com'
 license           'Apache 2.0'
 description       'Provides the shared messaging configuration for Chef for OpenStack.'
-version           '10.0.1'
+version           '10.0.2'
 
 recipe 'server', 'Installs and configures server packages for messaging queue used by the deployment.'
 recipe 'rabbitmq-server', 'Installs and configures RabbitMQ and is called via the server recipe'
